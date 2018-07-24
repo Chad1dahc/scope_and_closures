174594e7c1eaa37d06b9540a46458c348434aa9a
Chad Weeks <chadweeks@Chads-MacBook-Pro.local>
2018-07-23 21:05:55 -0400

    adding testing

--- src/leakyGlobals.js
+++ src/leakyGlobals.js
@@ -1,11 +1,4 @@
 // This is a simple file to show accessing global shit but like in node
-const assert = require('assert');
-
-name = 'outside'
-
-const showScopeByOverride = () => {
-	return global.name
-}
 
 const setVariable = (name, value) => {
 	global[name] = value
@@ -15,15 +8,7 @@ const getVariable = (name) => {
 	return global[name]
 }
 
-getVariableGlobal = (name) => {
-	return global[name]
+module.exports = {
+	setVariable,
+	getVariable
 }
-
-setVariable('chad', 'lil guy')
-setVariable('showScopeByOverride', showScopeByOverride)
-
-const whatIsChad = getVariable('chad')
-const eqWhatIsChad = global.getVariableGlobal('chad')
-
-assert.equal(whatIsChad, eqWhatIsChad, 'These should match, failure is a global issue.')
-assert.equal(showScopeByOverride(), global.showScopeByOverride(), 'These should match, failure is a global issue.')
