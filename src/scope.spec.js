89ac4114e4d6a694bce79f045b4c24afa316da35
hamoslo <h.weeks@gmail.com>
2018-07-24 20:32:35 -0700

    fixing up master

--- src/scope.spec.js
+++ src/scope.spec.js
@@ -1,18 +1,17 @@
 const {
-    firstObject,
-    secondObject,
-    nestedVarObject,
-    plainObject
+  firstObject,
+  nestedVarObject,
+  plainObject,
 } = require('./scope-ideas')
 
 describe('Scope Tests', () => {
-    it('should return the correct instance of bar', () => {
-        expect(firstObject.foo()).toBe('double')
-    })
-    it('should return the correct nested instance of bar', () => {
-        expect(nestedVarObject.foo.foo.foo()).toBe('double')
-    })
-    it('should return the correct instance of bar', () => {
-        expect(plainObject.foo.tiddler()).toBe('double')
-    })
+  it('should return the correct instance of bar', () => {
+    expect(firstObject.foo()).toBe('double')
+  })
+  it('should return the correct nested instance of bar', () => {
+    expect(nestedVarObject.foo.foo.foo()).toBe('double')
+  })
+  it('should return the correct instance of bar', () => {
+    expect(plainObject.foo.tiddler()).toBe('lol internal')
+  })
 })
